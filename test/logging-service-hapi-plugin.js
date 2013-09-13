41ab05bfe457702f9d09e1ec219ab75fcaaad23f
Alfio Zappala <azappala@azaptree.com>
2013-09-13 19:25:01 -0400

    tested plugin with 100% code coverage

--- test/logging-service-hapi-plugin.js
+++ test/logging-service-hapi-plugin.js
@@ -16,6 +16,7 @@
 'use strict';
 
 var expect = require('chai').expect;
+var Hapi = require('hapi');
 
 describe("LoggingService Hapi Plugin", function() {
 
@@ -26,7 +27,7 @@ describe("LoggingService Hapi Plugin", function() {
 			data : 'LoggingService can log events to console when no options are specified'
 		};
 		var i = 0;
-		
+
 		loggingService.log(event);
 		expect(loggingService.invalidEventCount).to.equal(0);
 
@@ -42,4 +43,183 @@ describe("LoggingService Hapi Plugin", function() {
 		expect(loggingService.invalidEventCount).to.equal(0);
 		expect(loggingService.eventCount).to.gt(0);
 	});
+
+	it("can be plugged into a Hapi pack with default options", function(done) {
+		var server = new Hapi.Server();
+
+		var loggingServiceOptions = {};
+
+		server.pack.require('../', loggingServiceOptions, function(err) {
+			expect(err).to.not.exist;
+			done();
+		});
+	});
+
+	it("can be plugged into a Hapi pack with options", function(done) {
+		var server = new Hapi.Server();
+
+		var options = {
+			loggingService : require('runrightfast-logging-service')()
+		};
+
+		options.loggingService.log({
+			tags : [ 'info' ],
+			data : 'loggingService.log type is : ' + (typeof options.loggingService.log)
+		});
+
+		server.pack.require('../', options, function(err) {
+			expect(err).to.not.exist;
+			done();
+		});
+	});
+
+	it("can be plugged into a Hapi pack with options into a specific server selected by label ", function(done) {
+		var server = new Hapi.Server({
+			labels : [ 'web' ]
+		});
+
+		var options = {
+			loggingService : require('runrightfast-logging-service')(),
+			serverLabels : [ 'web' ]
+		};
+
+		options.loggingService.log({
+			tags : [ 'info' ],
+			data : 'loggingService.log type is : ' + (typeof options.loggingService.log)
+		});
+
+		options.loggingService.log({
+			tags : [ 'info' ],
+			data : 'options.serverLabels is : ' + JSON.stringify(options.serverLabels)
+		});
+
+		server.pack.require('../', options, function(err) {
+			expect(err).to.not.exist;
+			done();
+		});
+	});
+
+	describe("registers a REST API at /log by default", function() {
+		it("- a single event can be POSTed to /log", function(done) {
+			var server = new Hapi.Server();
+
+			var loggingServiceOptions = {};
+
+			server.pack.require('../', loggingServiceOptions, function(err) {
+				expect(err).to.not.exist;
+
+				var event = {
+					tags : [ 'info' ],
+					data : 'test message'
+				};
+
+				server.inject({
+					method : 'POST',
+					url : '/log',
+					payload : JSON.stringify(event),
+					headers : {
+						'Content-Type' : 'application/json'
+					}
+				}, function(res) {
+					expect(res.statusCode).to.equal(202);
+					done();
+				});
+			});
+		});
+
+		it("- an array of events can be POSTed to /log", function(done) {
+			var server = new Hapi.Server();
+
+			var loggingServiceOptions = {};
+
+			server.pack.require('../', loggingServiceOptions, function(err) {
+				expect(err).to.not.exist;
+
+				var events = [ {
+					tags : [ 'info' ],
+					data : 'test message 1'
+				}, {
+					tags : [ 'info' ],
+					data : 'test message 2'
+				} ];
+
+				server.inject({
+					method : 'POST',
+					url : '/log',
+					payload : JSON.stringify(events),
+					headers : {
+						'Content-Type' : 'application/json'
+					}
+				}, function(res) {
+					expect(res.statusCode).to.equal(202);
+					done();
+				});
+			});
+		});
+
+		it("returns an HTTP response code of 400 when a single invalid event is POSTed", function(done) {
+			var server = new Hapi.Server();
+
+			var loggingServiceOptions = {};
+
+			server.pack.require('../', loggingServiceOptions, function(err) {
+				expect(err).to.not.exist;
+
+				var event = {
+					data : 'test message'
+				};
+
+				server.inject({
+					method : 'POST',
+					url : '/log',
+					payload : JSON.stringify(event),
+					headers : {
+						'Content-Type' : 'application/json'
+					}
+				}, function(res) {
+					expect(res.statusCode).to.equal(400);
+					done();
+				});
+			});
+		});
+
+		it("returns an HTTP response code of 400 when an array of events is POSTed containing an invalid event "
+				+ "- the number of valid events that were accepted are also returned", function(done) {
+			var server = new Hapi.Server();
+
+			var loggingServiceOptions = {};
+
+			server.pack.require('../', loggingServiceOptions, function(err) {
+				expect(err).to.not.exist;
+
+				var events = [ {
+					tags : [ 'info' ],
+					data : 'test message 1'
+				}, {
+					tags : [ 'info' ],
+					data : 'test message 2'
+				}, {
+					data : 'invalid message'
+				}, {
+					tags : [ 'info' ],
+					data : 'test message 3'
+				} ];
+
+				server.inject({
+					method : 'POST',
+					url : '/log',
+					payload : JSON.stringify(events),
+					headers : {
+						'Content-Type' : 'application/json'
+					}
+				}, function(res) {
+					expect(res.statusCode).to.equal(400);
+					var responseObject = JSON.parse(res.payload);
+					expect(responseObject.validCount).to.equal(2);
+					done();
+				});
+			});
+		});
+	});
+
 });
